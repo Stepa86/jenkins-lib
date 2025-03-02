8d8a7de784f05b1c6c5a65cacc8645d54f0b097b
Антон Степанов <a.stepanov@standart1c.ru>
2025-03-02 15:02:07 +0300

    фикс: Имя проекта EDT задается в srcDir

--- src/ru/pulsar/jenkins/library/edt/NativeEdtCliConverter.groovy
+++ src/ru/pulsar/jenkins/library/edt/NativeEdtCliConverter.groovy
@@ -19,14 +19,14 @@ class NativeEdtCliConverter implements IEdtCliEngine {
         String workspaceDir = FileUtils.getFilePath("$env.WORKSPACE/$EdtToDesignerFormatTransformation.WORKSPACE").getRemote()
         String projectWorkspaceDir = FileUtils.getFilePath("$workspaceDir/cf").getRemote()
         def configurationRoot = FileUtils.getFilePath("$env.WORKSPACE/$EdtToDesignerFormatTransformation.CONFIGURATION_DIR")
+        String projectDir = FileUtils.getFilePath("$env.WORKSPACE/$srcDir").getRemote()
         String configurationRootFullPath = configurationRoot.getRemote()
 
         Logger.println("Конвертация исходников конфигурации из формата EDT в формат Конфигуратора с помощью 1cedtcli")
 
         steps.deleteDir(configurationRoot)
 
-        def projectName = configurationRoot.getName()
-        def edtcliCommand = "1cedtcli -data \"$projectWorkspaceDir\" -command export --configuration-files \"$configurationRootFullPath\" --project-name \"$projectName\""
+        def edtcliCommand = "1cedtcli -data \"$projectWorkspaceDir\" -command export --configuration-files \"$configurationRootFullPath\" --project-name \"$projectDir\""
 
         steps.cmd(edtcliCommand)
 
